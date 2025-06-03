d08752c23628972f4af7f9a35479ca8a5aef7d09
Souradip Adhikari <souradip.cloud@gmail.com>
2025-06-03 23:39:51 +0530

    working on security

---
+++ VotingApp/src/main/java/com/vote/VotingApp/Config.java
@@ -0,0 +1,23 @@
+package com.vote.VotingApp;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class Config {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests((request)->request
+                .requestMatchers("/api/voters/*","/api/votes/*","/api/election-result/*").authenticated()
+                .requestMatchers("/api/candidate/*").permitAll()
+        );
+        httpSecurity.formLogin(Customizer.withDefaults());
+        httpSecurity.httpBasic(Customizer.withDefaults());
+
+    return httpSecurity.build();
+    }
+
+}
