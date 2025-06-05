eefd6de46153340fbeeeda9ffd736b69c864ae9e
Souradip Adhikari <souradip.cloud@gmail.com>
2025-06-06 00:24:56 +0530

    Making it stateless

--- VotingApp/src/main/java/com/vote/VotingApp/Config.java
+++ VotingApp/src/main/java/com/vote/VotingApp/Config.java
@@ -4,17 +4,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class Config {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        //
+
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+
+        //
+
         httpSecurity.authorizeHttpRequests((request)->request
                 .requestMatchers("/api/voters/*","/api/votes/*","/api/election-result/*").authenticated()
                 .requestMatchers("/api/candidate/*").permitAll()
         );
-        httpSecurity.formLogin(Customizer.withDefaults());
+        httpSecurity.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+//        httpSecurity.formLogin(Customizer.withDefaults());
         httpSecurity.httpBasic(Customizer.withDefaults());
 
     return httpSecurity.build();
